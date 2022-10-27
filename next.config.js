54b89111f88e9734c7d6266ddd2701c6ae61ac5d
Abdullah-Mehmood-bit <abdullahgithub007@gmail.com>
2022-10-27 12:19:26 -0700

    done

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.infoanimalscare.com/?p=167&fbclid=IwAR09OFgBapd_rzfl_BFQTkx1OTGAhaxakeg4C5eCdTZK-MYlpWpNTRJ1hxU',
+        permanent: false
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
